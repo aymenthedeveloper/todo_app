306f97171fa2753ed7d32ea500d4fd1b4d3e3288
aymenthedeveloper <aymendev.contact@gmail.com>
2023-12-06 00:25:52 +0000

    added display by day feature

--- main/models.py
+++ main/models.py
@@ -1,11 +1,21 @@
 from django.db import models
 
+
+class Date(models.Model):
+    now = models.DateField()
+
+    class Meta:
+        ordering = ['-now']
+
+    def __str__(self) -> str:
+        return self.now.strftime("%d %m %Y")
+
 # Create your models here.
 class Task(models.Model):
     name = models.CharField(max_length=255)
     completed = models.BooleanField(default=False)
-    created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    date = models.ForeignKey(Date, null=True, on_delete=models.SET_NULL)
 
     def __str__(self) -> str:
         return f"{self.name} completed={self.completed}"
--- main/views.py
+++ main/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
-from .models import Task
+from .models import Task, Date
 from .forms import TaskForm
+from datetime import date
 
 # Create your views here.
-def home(request):
+def home_(request):
     q = request.GET.get('q')
     if q:
         tasks = Task.objects.filter(completed=q)
@@ -17,6 +18,28 @@ def home(request):
             return redirect('home')
     context = {'tasks': tasks, 'form':form}
     return render(request, 'main/index.html', context)
+
+def home(request):
+    q = request.GET.get('q')
+    if q:
+        tasks = {f'completed={q}':Task.objects.filter(completed=q)}
+    else:
+        dates = Date.objects.all()
+        tasks = {f"{date_today}":date_today.task_set.all() for date_today in dates}
+    form = TaskForm()
+    if request.method == 'POST':
+        task = TaskForm(request.POST)
+        if task.is_valid():
+            task = task.save(commit=False)
+            today = Date.objects.filter(now=date.today())
+            if today:
+                task.date = today[0]
+            else:
+                task.date = Date.objects.create(now=date.today())
+            task.save()
+            return redirect('home')
+    context = {'tasks': tasks, 'form':form}
+    return render(request, 'main/index_copy.html', context)
 
 
 
